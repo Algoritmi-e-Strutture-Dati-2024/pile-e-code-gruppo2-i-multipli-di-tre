1f268473d381f3832db7ab86e3dd29c47145df84
GianmarcoDiRusso <g.dirusso@studenti.uniba.it>
2024-12-06 11:39:16 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,32 @@
+	int main(){
+			
+			
+			
+			pila<int>pilamia;
+			coda<int>codamia;
+			
+			int lung;
+			int e;
+				
+				
+				
+			for(int i=0;i<a;i++){
+				std::cout<<"inserisci un elemento"<< std::endl;
+				std::cin>>e
+				pilamia.push;
+				
+			};
+			
+		
+				std::cout<<"inserisci un elemento"<<std::endl;
+				std::cin>>elem;
+				codamia.enqueue;
+		
+			
+			
+			
+		
+			
+			
+			
+		}
